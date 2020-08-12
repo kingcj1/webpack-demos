ea9ddc9254488fca2b1e57e56de9b57da93c0dd6
cheang king <cheangking@cheangdeMacBook.local>
2020-08-12 22:55:25 +0800

    测试输出

--- demo01/webpack.config.js
+++ demo01/webpack.config.js
@@ -4,6 +4,7 @@
  */
 
 const path = require("path");
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -12,6 +13,9 @@ module.exports = {
   output: {
     publicPath: __dirname + "/dist/", // js引用路径或者CDN地址
     path: path.resolve(__dirname, "dist"), // 打包文件的输出目录
-    filename: "bundle.js"
-  }
+    filename: '[name].[hash].js'
+  },
+  plugins: [    
+    new CleanWebpackPlugin()  
+]
 };
